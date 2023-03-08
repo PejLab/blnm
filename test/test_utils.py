a64ba06104a3574055a71a214462c0b1a91c4f1a
Robert Vogel <rvogel@scripps.edu>
2023-03-07 16:37:29 -0800

    Added utils.logistic unit-test.

--- test/test_utils.py
+++ test/test_utils.py
@@ -4,12 +4,13 @@ import numpy as np
 from blnm import utils
 
 
-class TestBIC(TestCase):
+class TestMbic(TestCase):
     log_likelihood = -1
     n_samples = 100
     k_mixtures = 5
+    bic = 2*k_mixtures*np.log(n_samples) - 2*log_likelihood
 
-    def test_input_sanitize(self):
+    def test_exceptions(self):
         with self.assertRaises(ValueError):
             utils.mbic(self.log_likelihood,
                       self.n_samples,
@@ -29,6 +30,29 @@ class TestBIC(TestCase):
             utils.mbic(np.array([-1,-2,-3]),
                       self.n_samples,
                       self.k_mixtures)
+
+    def test_output(self):
+        self.assertEqual(utils.mbic(np.nan, 1000, 3), np.nan)
+
+        self.assertEqual(utils.mbic(self.log_likelihood,
+                                    self.n_samples,
+                                    self.k_mixtures),
+                         self.bic)
+
+
+class TestLogistic(TestCase):
+    def test_output(self):
+        self.assertAlmostEqual(utils.logistic(np.log(2)), 2/3)
+
+        self.assertAlmostEqual(utils.logistic(-np.log(2)), 1/3)
+
+        s = np.full(100, -np.log(2))
+        out = utils.logistic(s)
+
+        self.assertEqual(out.size, s.size)
+
+        for val in out:
+            self.assertAlmostEqual(val, 1/3)
 
 
 
